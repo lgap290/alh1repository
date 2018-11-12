ef8c10c76033347dbb272937ce6fe8c8ccb385d8
Luis Gabriel Avella <luis.avellap@gmail.com>
2018-11-12 07:28:25 -0500

    Dashboard Admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
+use Carbon\Carbon;
+use DB;
+
 /**
  * Class HomeController
  * @package App\Http\Controllers
@@ -33,6 +36,32 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $total_hoy = DB::table('alc_botellaslicor')
+            ->where('created_at', '>=', Carbon::today())
+            ->sum('n_consultas');
+        $total = DB::table('alc_botellaslicor')
+            ->sum('n_consultas');
+        return view('home', ['total'=>$total, 'total_hoy'=>$total_hoy]);
+    }
+
+    public function chartjs()
+    {
+    /*     $viewer = View::select(DB::raw("SUM(numberofview) as count"))
+            ->orderBy("created_at")
+            ->groupBy(DB::raw("year(created_at)"))
+            ->get()->toArray();
+
+        $viewer = array_column($viewer, 'count');
+
+        $click = Click::select(DB::raw("SUM(numberofclick) as count"))
+            ->orderBy("created_at")
+            ->groupBy(DB::raw("year(created_at)"))
+            ->get()->toArray();
+        $click = array_column($click, 'count');
+
+        return view('chartjs')
+                ->with('viewer',json_encode($viewer,JSON_NUMERIC_CHECK))
+                ->with('click',json_encode($click,JSON_NUMERIC_CHECK)); */
+
     }
 }
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,24 +6,8 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Adminlte-laravel - {{ trans('adminlte_lang::message.landingdescription') }} ">
-    <meta name="author" content="Sergi Tur Badenas - acacha.org">
 
-    <meta property="og:title" content="Adminlte-laravel" />
-    <meta property="og:type" content="website" />
-    <meta property="og:description" content="Adminlte-laravel - {{ trans('adminlte_lang::message.landingdescription') }}" />
-    <meta property="og:url" content="http://demo.adminlte.acacha.org/" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE.png" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x600.png" />
-    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x314.png" />
-    <meta property="og:sitename" content="demo.adminlte.acacha.org" />
-    <meta property="og:url" content="http://demo.adminlte.acacha.org" />
-
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@acachawiki" />
-    <meta name="twitter:creator" content="@acacha1" />
-
-    <title>{{ trans('adminlte_lang::message.landingdescriptionpratt') }}</title>
+    <title>IDES - SIVA</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">

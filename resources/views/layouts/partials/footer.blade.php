ab910cd7835db8534c8f60b31567cfd2a76a12a5
Luis Gabriel Avella <luis.avellap@gmail.com>
2018-11-20 08:09:20 -0500

    cambios appstore icon

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -4,7 +4,12 @@
         <a href='https://play.google.com/store/apps/details?id=com.lmtcolombia.gabriel.ides1&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
             <img alt='Disponible en Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/es-419_badge_web_generic.png' width="200"/>
         </a>
+        <a href='#'>
+            <img alt='Disponible en AppStore' src='https://developer.apple.com/app-store/marketing/guidelines/images/badge-example-alternate.png' width="200"/>
+        </a>
     </div>
+
+    
     <!-- To the right -->
     <!-- Default to the left -->
     <strong>Copyright &copy; 2018 IDES </a>
